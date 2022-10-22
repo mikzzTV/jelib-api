9a1c490dd149937c93a0b6ed28ee191f4300b7f9
Viktor <viktorf1999@mail.ru>
2022-10-22 19:19:58 +0300

    Updated

--- src/main/java/ru/popis/jeapi/api/player/PlayerWrapper.java
+++ src/main/java/ru/popis/jeapi/api/player/PlayerWrapper.java
@@ -3,5 +3,5 @@ package ru.popis.jeapi.api.player;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-public interface PlayerWrapper extends CommandSender, Player {
+public interface PlayerWrapper extends CommandSender {
 }
